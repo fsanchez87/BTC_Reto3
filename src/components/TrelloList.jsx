693a847f16aa5c2cf8e5a501c9d9f972bbcbf746
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-08-11 13:05:20 +0200

    add root styles

--- src/components/TrelloList.jsx
+++ src/components/TrelloList.jsx
@@ -1,17 +1,29 @@
-import {Paper, CssBaseline} from "@material-ui/core"
+import {Paper, CssBaseline, makeStyles} from "@material-ui/core"
 import AddCardorList from "./AddCardorList"
 import ListTitle from "./ListTitle"
 import TrelloCard from "./TrelloCard"
 
 const TrelloList = () => {
+    const classes = useStyles();
     return (
-        <Paper>
+        <Paper className={classes.root}>
             <CssBaseline/>
             <ListTitle/>
+            <TrelloCard/>
+            <TrelloCard/>
             <TrelloCard/>
             <AddCardorList/>  
         </Paper>
     )
 }
+
+const useStyles = makeStyles(theme =>({
+    root: {
+        width: "300px",
+        background: "#ebecf0",
+        margin: theme.spacing(1)
+
+    }
+}))
 
 export default TrelloList
