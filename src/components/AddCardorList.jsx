17ae277f1008f38d5e8f1ea2efbf894d75ac445b
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-08-11 20:18:37 +0200

    edit c AddCardorList

--- src/components/AddCardorList.jsx
+++ src/components/AddCardorList.jsx
@@ -10,16 +10,34 @@ import AddCardorListText from "./AddCardorListText";
 
 const AddCardorList = () => {
   const [open, setOpen] = useState(false);
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <Collapse in={open}>
         <AddCardorListText />
       </Collapse>
       <Collapse in={!open}>
-        <h1>Cerrado</h1>
+        <Paper className={classes.addCardorListText}>
+          <Typography>+ Add a card</Typography>
+        </Paper>
       </Collapse>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "300px",
+    marginTop: theme.spacing(1),
+  },
+  addCardorListText: {
+    padding: theme.spacing(1, 1, 1, 2),
+    margin: theme.spacing(0, 1, 1, 1),
+    background: "#ebecf0",
+    "&:hover":{
+        backgroundColor: fade("#000", 0.25)
+    }
+  },
+}));
 
 export default AddCardorList;
