2d98789111f5ac0fe6c63b107259b38261424794
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-08-11 13:40:07 +0200

    edit styles listTitle

--- src/components/ListTitle.jsx
+++ src/components/ListTitle.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import {makeStyles, Typography} from "@material-ui/core";
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const ListTitle = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>To do</h1>
+        <div className={classes.title}>
+            <Typography className={classes.titleText}>
+                To do
+            </Typography>
+            <MoreHorizIcon/>
         </div>
+
     )
 }
+
+const useStyles = makeStyles(theme =>({
+    title: {
+        display: "flex",
+        margin: theme.spacing(1),
+
+    },
+    titleText: {
+        flexGrow: 1,
+        fontSize: "1.2rem",
+        fontWeight: "bold"
+    }
+}))
+
 
 export default ListTitle
