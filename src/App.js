99cf73d23f1ad398d6724839a97908427ad61c13
Francisco Sánchez <sanchezgarridof@gmail.com>
2021-08-18 00:22:05 +0200

    add classes App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-import TrelloList from './components/TrelloList';
+import "./App.css";
+import TrelloList from "./components/TrelloList";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <TrelloList/>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <TrelloList />
+        <TrelloList />
+        <TrelloList />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    overflowY: "auto",
+  },
+  container:{
+    display: "flex"
+  }
+}));
